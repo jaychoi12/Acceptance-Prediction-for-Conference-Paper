7864416d661b093fc42a81d01451a82e2a4779bd
dkswodus49 <dkswodus49@kaist.ac.kr>
2020-06-01 14:55:01 +0900

    [Add] crawling_jy initial

---
+++ crawling_jy.py
@@ -0,0 +1,46 @@
+from selenium import webdriver 
+from pyvirtualdisplay import Display
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import csv
+
+display = Display(visible=0, size=(1024, 768)) 
+display.start()
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('disable-gpu')
+
+#path = '/home/jaychoi/utils/chromedriver'
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+wait = WebDriverWait(driver, 60)
+
+url = 'https://openreview.net/group?id=ICLR.cc/2020/Conference#accept-poster'
+driver.get(url)
+
+tab = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="accept-poster"]/ul')))
+xpath = '//*[@id="accept-poster"]/ul/li[2]/div[@class="collapse"]/div/ul'
+ullist = driver.find_element_by_xpath(xpath)
+cats = ullist.find_elements(By.tagName("li"))
+print(cats.size())
+
+'''
+with open('/tmp/output.tsv', 'wt') as out_file:
+    tsv_writer = csv.writer(out_file, delimiter='\t')
+    i = 0 #index for paper
+    while True:
+        xpath_a = '//*[@id="accept-poster"]/ul/li[%d]/div[@class="collapse in"]/div/ul/li[2]'%i
+        abstract = driver.find_element_by_xpath(xpath_a)
+        xpath_t = '//*[@id="accept-poster"]/ul/li[%d]/div[@class="collapse in"]/div/ul/li[1]'%i
+        tl_dr = driver.find_element_by_xpath(xpath_t)
+        xpath_k = '//*[@id="accept-poster"]/ul/li[%d]/div[@class="collapse in"]/div/ul/li[3]'%i
+        keywords = driver.find_element_by_xpath(xpath_k)
+        
+        i += 1
+        if abstract == None:
+            break
+'''
+driver.quit()
--- crawling_selenium.py
+++ crawling_selenium.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 
 display = Display(visible=0, size=(1024, 768)) 
 display.start()
@@ -12,10 +13,9 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('disable-gpu')
 
-path = '/home/jaychoi/utils/chromedriver' 
-driver = webdriver.Chrome(path, options=options)
+#path = '/home/jaychoi/utils/chromedriver'
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 wait = WebDriverWait(driver, 30)
-
 url = 'https://openreview.net/group?id=ICLR.cc/2020/Conference#accept-poster'
 driver.get(url)
 
