1a5bf5f63c90cc77b694e9e0eec75a9023d6db26
dkswodus49 <dkswodus49@kaist.ac.kr>
2020-06-01 22:54:35 +0900

    [Add] crawling for 2020

--- crawling_jy.py
+++ crawling_jy.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-#from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 import csv
 
 display = Display(visible=0, size=(1024, 768)) 
@@ -14,17 +14,17 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('disable-gpu')
 
-path = '/home/jaychoi/utils/chromedriver'
-driver = webdriver.Chrome(path, options=options)
-#driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-wait = WebDriverWait(driver, 60)
+#path = '/home/jaychoi/utils/chromedriver'
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+wait = WebDriverWait(driver, 300)
+
 
 url = 'https://openreview.net/group?id=ICLR.cc/2020/Conference#accept-poster'
 driver.get(url)
 
 tab = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="accept-poster"]/ul')))
 
-with open('output_acceptposter.tsv', 'wt') as out_file:
+with open('output_2020.tsv', 'wt') as out_file:
     tsv_writer = csv.writer(out_file, delimiter='\t')
     tsv_writer.writerow(['title', 'accept', 'abstract', 'tldr', 'keyword', 'code'])
     
