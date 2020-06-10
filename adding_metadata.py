6f588d9b245d2c1acc5ff2074b3e46fe5370b120
jaychoi12 <jaychoi@postech.ac.kr>
2020-06-10 14:23:08 +0900

    [Add] vocab from nltk corpus, to check oov

--- adding_metadata.py
+++ adding_metadata.py
@@ -1,6 +1,7 @@
 import csv
 import pickle
-
+import sys
+import re
 
 with open('abstract_vocab.pickle', 'rb') as handle:
     vocab_dict = pickle.load(handle)
@@ -15,19 +16,33 @@ with open('./output_2020.tsv','r') as f_in:
         header.extend(metadata_list)
         writer.writerow(header)
         #['title', 'accept', 'abstract', 'tldr', 'keyword', 'code']
+        avg_acc = 0
+        cnt_acc = 0
+        avg_rej = 0
+        cnt_rej = 0
         for row in reader:
             addition = [None]*6
             row[0] = ' '.join(row[0].split()) #reprocessing the title..since it has a line break 
             addition[0] = len(row[0].split()) #title length
+            if row[1] == '1':
+                cnt_acc += 1
+            else:
+                cnt_rej += 1
             addition[1] = len(row[2].split()) #abstract length
             addition[2] = len(row[4].split(',')) if row[4] != '' else 0 #num of keyword
             addition[3] = 1 if row[5] != '' else 0 #is code
             addition[4] = len(row[3].split()) if row[3] != '' else 0 #tldr length
-            addition[5] = 0
-            for word in row[2].split():
-                if vocab_dict[word] < 2:
+            addition[5] = 0 #num of oov
+            for word in re.findall(r"[\w]+", row[2]):
+                word = word.lower()
+                if word in vocab_dict and vocab_dict[word] == 1:
                     addition[5] += 1
+                    if row[1] == '1':
+                        avg_acc += 1
+                    else:
+                        avg_rej += 1
 
             row.extend(addition)
             writer.writerow(row)
-            
+        print(avg_acc / cnt_acc)
+        print(avg_rej / cnt_rej)
--- build_vocab.py
+++ build_vocab.py
@@ -1,7 +1,16 @@
 import csv
 import pickle
 import re
+from nltk.corpus import gutenberg
 
+nltk_vocab = []
+for fileid in gutenberg.fileids():
+    for sent in gutenberg.sents(fileid):
+        for word in re.findall(r"[\w]+", ' '.join(sent)):
+            word = word.lower()
+            nltk_vocab.append(word)
+            nltk_vocab = list(set(nltk_vocab))
+print(len(nltk_vocab))
 vocab_dict = {}
 year_list = [2018, 2019, 2020]
 except_list = ['(', ')', 'http', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
@@ -18,7 +27,7 @@ for year in year_list:
                     if except_character in word:
                         flag = 1
                         break
-                if flag == 0:
+                if flag == 0 and word not in nltk_vocab:
                     if word in vocab_dict:
                         vocab_dict[word] += 1
                     else:
@@ -29,7 +38,7 @@ cnt = 0
 for k, v in vocab_dict.items():
     if v == 1:
         cnt += 1
-        print(k)
+        #print(k)
 print(cnt)
 
 with open('abstract_vocab.pickle', 'wb') as handle:
